e4fe8f97df39b10dbda6bbaf6fe34abe59afeea0
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-09-11 20:04:04 -0500

    Add testing tools

--- test/dog.js
+++ test/dog.js
@@ -5,11 +5,11 @@ describe('Dog', function () {
     var booljs      = require('bool.js')
     ,   chai        = require('chai')
     ,   asPromised  = require('chai-as-promised')
-    ,   app, Dog, dogDao;
+    ,   supertest   = require('supertest-as-promised')
+    ,   app, Dog, dogDao, agent;
 
     chai.use(asPromised);
     var expect      = chai.expect;
-
 
     before(function () {
         return booljs('com.example.api')
@@ -17,18 +17,49 @@ describe('Dog', function () {
         .run().then(function (api) {
             app = api.app;
             Dog = new app.models.Dog();
+            dogDao = new app.dao.Dog();
+            agent = supertest(api.server);
             return q.resolve();
         });
     });
 
     describe('Model', function () {
 
-        it('cleans the collection', function () {
+        before(function () {
             return q.nbind(Dog.collection.remove, Dog.collection)();
         });
 
         it('retrieves an empty list', function () {
             return expect(Dog.list()).to.eventually.have.length(0);
+        });
+
+    });
+
+    describe('DAO', function () {
+
+        before(function () {
+            return q.nbind(Dog.collection.remove, Dog.collection)();
+        });
+
+        it('retrieves an empty list', function () {
+            return expect(dogDao.list()).to.eventually.have.length(0);
+        });
+
+    });
+
+    describe('Controller', function () {
+
+        before(function () {
+            return q.nbind(Dog.collection.remove, Dog.collection)();
+        });
+
+        it('retrieves an empty list', function () {
+
+            return agent.get('/dog').expect(200).then(function (response) {
+                expect(response.body.data).to.have.length(0);
+            });
+
+
         });
 
     });
